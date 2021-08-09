5efb10b682581bec64f3bfbe6082657386bbfd3a
名字 <xxxx@xx.com>
2021-08-09 14:20:52 +0800

    更改

--- index.js
+++ index.js
@@ -3,34 +3,34 @@ function whichButton(event) {
     if (btnNum == 2) {
         console.log("你点击了鼠标右键！")
     } else if (btnNum == 0) {
-        // let timer = true;
-        // if (timer) {
-        //     $('.canvas1').addClass('canvas2');　　
-        //     setTimeout(function() {　　　　
-        //         $('.canvas2').css('animation', 'bar-grow2 1s 1');　　
-        //     }, 1000);
+        let timer = true;
+        if (timer) {
+            $('.canvas1').addClass('canvas2');　　
+            setTimeout(function() {　　　　
+                $('.canvas2').css('animation', 'bar-grow2 1s 1');　　
+            }, 1000);
 
-        //     timer = false
-        // }
+            timer = false
+        }
 
-        // $('.canvas2').css('animation', 'bar-grow 1s 1');　　
+        $('.canvas2').css('animation', 'bar-grow 1s 1');　　
 
 
         console.log("你点击了鼠标左键！")
     } else if (btnNum == 1) {
         console.log("你点击了鼠标中键！");
 
-        let timer = true;
-        if (timer) {
-            $('.canvas1').addClass('canvas3');　　
-            setTimeout(function() {　　　　
-                $('.canvas2').css('animation', 'bar-grow 1s 1');　　
-            }, 1000);
+        // let timer = true;
+        // if (timer) {
+        //     $('.canvas1').addClass('canvas3');　　
+        //     setTimeout(function() {　　　　
+        //         $('.canvas2').css('animation', 'bar-grow 1s 1');　　
+        //     }, 1000);
 
-            timer = false
-        }
+        //     timer = false
+        // }
 
-        $('.canvas2').css('animation', 'bar-grow2 1s 1');　　
+        // $('.canvas2').css('animation', 'bar-grow2 1s 1');　　
 
     } else {
         console.log("你点击了" + btnNum + "号键，我不能确定它的名称。");
